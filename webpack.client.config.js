9a36900ec91226fadc52272b5c4630e14b67e6d5
孙志奇 <769053649@qq.com>
2019-03-27 18:27:33 +0800

    edit

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,10 +1,15 @@
 
-
+const path = require('path');
 module.exports={
     mode:"development",
     entry:'./src/client/index',
     output:{
         filename:"[name].js",
-        path:__dirname+'/dist',
+        path:path.join(__dirname,'/dist'),
+    },
+    devServer:{
+        port:3000,
+        contentBase:path.join(__dirname,'/dist'),
+        compress: true,
     },
 }
